1431ad943fb9e21695969b7031db5b850d971738
albert3456-1 <yund46237@gmail.com>
2026-05-28 01:32:31 +0900

    week2 실습

---
+++ week2/scripts.js
@@ -0,0 +1,32 @@
+
+
+let root = document.getElementById("root");
+let postsButton = document.getElementById("posts");
+
+postsButton.addEventListener("click",onButtonClick)
+
+function onButtonClick(){
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+
+    .then((response) => response.json())
+    .then((json) => render(json));
+
+}
+
+function render(data) {
+    for(let i=0; i<data.length;i++){
+
+        let h2 = document.createElement("h2");
+        let p = document.createElement("p");
+
+        h2.innerText=data[i].title;
+        p.innerText=data[i].body;
+
+        root.appendChild(h2);
+        root.appendChild(p);
+    }
+
+}
+
+
+
